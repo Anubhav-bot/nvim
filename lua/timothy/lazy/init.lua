df800868fbaed497516f9a10305cd5021de1dc8c
Anubhav <pubgplayer7014@gmail.com>
2024-03-25 19:33:43 +0545

    treesitter

--- lua/timothy/lazy/init.lua
+++ lua/timothy/lazy/init.lua
@@ -21,6 +21,16 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        config = function ()
+            local configs = require("nvim-treesitter.configs")
+
+            configs.setup({
+                ensure_installed = { "c", "lua", "vue", "vim", "vimdoc", "javascript", "html", "css"},
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
     },
 
     {
--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -2,7 +2,5 @@ local builtin = require('telescope.builtin')
 
 return {
     vim.keymap.set('n', "<leader>pf", builtin.find_files, {} );
+    vim.keymap.set('n', "<leader>ps", builtin.live_grep, {} );
 }
-
-
-
